c363443218c3b6a5fc3281a06fc3737c6c6a8d64
ParasThakur199 <parasjamwal19@gmail.com>
2023-06-16 00:43:01 +0530

    add serviceImpl

--- OnlineShopping/src/main/java/com/shopping/service/OrderServiceImpl.java
+++ OnlineShopping/src/main/java/com/shopping/service/OrderServiceImpl.java
@@ -1,5 +1,72 @@
 package com.shopping.service;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.shopping.model.Order;
+import com.shopping.model.User;
+import com.shopping.repository.OrderRepository;
+
+@Service
 public class OrderServiceImpl implements OrderService {
+	
+	@Autowired
+	private OrderRepository orderRepository;
+
+	@Override
+	public List<Order> getAllOrders() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Order getOrderById(Integer orderId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Order> getOrdersByUser(User user) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Order createOrder(Order order) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Order updateOrder(Order order) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void deleteOrder(Integer orderId) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void cancelOrder(Order order) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void processOrder(Order order) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void completeOrder(Order order) {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
